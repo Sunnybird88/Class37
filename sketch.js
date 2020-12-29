5fe1e4aa5723709e13965dac865b7bcf91a25486
Sunnybird88 <69608484+Sunnybird88@users.noreply.github.com>
2020-12-28 19:05:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var database
+var form, player, game
+var gameState = 0
+var playerCount
+var allPlayers
+var distance = 0
+
+
+function setup(){
+    createCanvas(500, 500);
+
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+
+    if (playerCount === 4) {
+        game.update(1)
+    }
+    if (gameState === 1) {
+        clear();
+        game.play();
+    }
+
+}
